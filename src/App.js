d1b09989c742af76079394ec7ba18839836d4bf7
Maryia Biandeha <yauheniznak@gmail.com>
2023-01-03 20:34:04 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from "react";
+import {data} from './data'
 
 function App() {
+
+  const [places,setPLaces] = useState(data);
+  const [showText,setShowText] = useState(false)
+
+  const removeItem = (id) =>{
+    let newPlace = places.filter(place=>
+    place.id !== id);
+    setPLaces(newPlace)
+  }
+  
+
+   const showTextClick =(element) =>{
+    element.showMore = !element.showMore
+    setShowText (!showText)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+      <div className="container" >
+        <h1>{places.length} World's Best Places to Visit</h1>
     </div>
+
+    {places.map ((element =>{
+      const {id,place,image,description,showMore} = element;
+
+      return (
+        <div className="container" key={id}>
+          <div className="container">
+            <h3>{id}-{place}</h3>
+          </div>
+          <div className="container">
+            <img src={image} width="300px" height="300px" alt="places"/>
+          </div>
+          <div className="container">
+            <p>{showText?description:description.substring(0,200)+"..."}
+            <button onClick ={()=> {showTextClick(element)} }>{showMore?"Show More":"Show Less"}</button>
+            </p>
+            
+          </div>
+         <div className="btn">
+           <button onClick={()=>{removeItem(id)}}>Delete</button>
+         </div>
+
+        </div>
+      )
+
+
+    }))}
+     <div className="container">
+            <button onClick ={()=>setPLaces([])}>DELETE ALL</button>
+          </div>
+  </div>
+   
+ 
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,51 @@
+export const data =[
+    {
+        id:1,
+        place:"South Island, New Zealand",
+        image:"https://images.unsplash.com/photo-1666615611646-1d9275e82fb1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmV3JTIwemVhbGFuZCUyMHNvdXRoJTIwaXNsYW5kfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+        description:"New Zealand's South Island brims with majestic landscapes at every turn, from dramatic mountains to spectacular fjords. Here, you can admire the mountains of Fiordland National Park, a UNESCO World Heritage Site, from hiking trails or a boat on Milford Sound. At night, journey to the University of Canterbury's Mount John Observatory to gaze at the starry skies. You can also indulge your inner daredevil in Queenstown (a favorite destination among bungee jumpers), sample delicious sauvignon blanc wine in the Marlborough region or explore New Zealand's most accessible glaciers – Franz Josef and Fox Glacier – on the island's west coast.",
+        showMore:"false",
+    },
+    {
+        id:2,
+        place:"Paris",
+        image:"https://images.unsplash.com/photo-1522093007474-d86e9bf7ba6f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cGFyaXN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+        description:"The magnetic City of Light draws visitors from around the globe who come to see iconic attractions like the Eiffel Tower, the Louvre and the Arc de Triomphe. But what travelers really fall in love with are the city's quaint cafes, vibrant markets, trendy shopping districts and unmistakable je ne sais quoi. Get lost wandering along the charming cobblestone streets, or grab a croissant and relax on the banks of the Seine for hours. If you're up for a quick daytrip, head about 15 miles southwest of the city center to the Palace of Versailles, which offers guided and self-guided tours of the estate.",
+        showMore:"false",
+    },
+    {
+        id:3,
+        place:"Maui",
+        image:"https://images.unsplash.com/photo-1562191326-0da0767cfffe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8TWF1aXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+        description:"Whether you're driving along the Road to Hana, enjoying a bird's-eye view of Maui's lush coastline from a helicopter, snorkeling with sea turtles or simply relaxing on white or black sand beaches, you'll find that this Hawaiian island is unlike any other tropical destination. Don't miss a chance to visit Haleakala National Park, which is home to one of the world's largest dormant volcanic craters. You should also attend a luau for a dose of local culture and a taste of Hawaiian specialties like poi, poke and mahimahi.",
+        showMore:"false",
+    },  
+    {
+        id:4,
+        place:"Bora Bora",
+        image:"https://images.unsplash.com/photo-1633381659744-d4804482b548?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fEJvcmElMjBCb3JhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+        description:"What this small French Polynesian island may lack in size it makes up for in sheer tropical beauty. Here, you'll find picturesque beaches, lush jungles and luxurious resorts. The island's extinct volcano, Mount Otemanu, makes for a great photo-op or challenging hike, and the friendly Bora Bora locals can help you catch a glimpse of the island's best sights during an off-road excursion. To relax, head to Matira Beach for crystal-clear water and soft white sand. Although a trip to Bora Bora is very expensive, most travelers say it's worth every penny.",
+        showMore:"false",
+    },
+    {
+        id:5,
+        place:"Tahiti",
+        image:"https://images.unsplash.com/photo-1563177681-1b54d557f053?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8VGFoaXRpfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+        description:"Travel to this island – the largest in French Polynesia – if you've been dreaming of a vacation spent lazing in a lavish overwater bungalow. Beyond the posh resorts, Tahiti boasts black sand and golden beaches, a bustling capital and prime snorkeling and surfing conditions. If you're looking for more cultural experiences, check out some of the island's ancient temples or shop at its traditional indoor markets. To save money, opt to stay in a vacation rental – the island is home to plenty of cute bungalows and chic condos",
+        showMore:"false",
+    },
+    {
+        id:6,
+        place:"London",
+        image:"https://images.unsplash.com/photo-1526129318478-62ed807ebdf9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bG9uZG9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+        description:"London is a world unto itself. The eclectic neighborhoods, which are home to a blend of historical landmarks and modern-day attractions, can keep you occupied for days. If it's your first time in London, plan to see the top spots, such as the Tower of London, the Tate Modern art institution, Buckingham Palace, Borough Market and the British Museum, before sitting down to a classic afternoon tea or checking out a local pub. The best time to travel to London is during the warmer months, but be warned that this is also the busiest and most expensive time of year.",
+        showMore:"false",
+    },
+    {
+        id:7,
+        place:"Rome",
+        image:"https://images.unsplash.com/photo-1555992828-ca4dbe41d294?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHJvbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+        description:"When you visit the Eternal City, prepare to cross a few must-see attractions – including the Colosseum, the Trevi Fountain and the Pantheon – off of your bucket list. Travelers can see additional treasures, such as St. Peter's Basilica and the Sistine Chapel, in nearby Vatican City. Escape the tourist crowds by taking a twilight stroll along the cobblestone streets of Trastevere, or head to Mercato Centrale Roma to sample local delicacies like gelato and pizza. Before leaving, peruse some of Rome's lesser-known museums, art galleries and boutiques",
+        showMore:"false"
+    }
+]
